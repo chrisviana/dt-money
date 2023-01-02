bfc173f92baf08ea2f038478647949e946321a1f
Christian <viana2015christian@gmail.com>
2023-01-02 08:02:13 -0300

    Realizando requisição HTPP

--- src/pages/Transctions/index.tsx
+++ src/pages/Transctions/index.tsx
@@ -1,41 +1,61 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransctionsContainer, TransctionsTable } from "./styles";
+import {
+  PriceHighlight,
+  TransctionsContainer,
+  TransctionsTable,
+} from "./styles";
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createAt: string;
+}
 
 export function Trasctions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
-            <TransctionsContainer>
-                <SearchForm />
-                <TransctionsTable>
-                    <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento ded site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Hamburguer</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2022</td>
-                        </tr>
-                    </tbody>
-                </TransctionsTable>
-            </TransctionsContainer>
+  const [transctions, setTransactions] = useState<Transaction[]>([]);
 
-        </div>
-    )
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransctionsContainer>
+        <SearchForm />
+        <TransctionsTable>
+          <tbody>
+            {transctions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </TransctionsTable>
+      </TransctionsContainer>
+    </div>
+  );
 }
